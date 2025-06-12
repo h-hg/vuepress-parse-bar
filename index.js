3f5c3a4e9c4c362cb54b9975ad63d1efa6ba9c4e
h-hg <1394564544@qq.com>
2025-06-13 01:12:30 +0800

    format, fix error path of root

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ import fs from 'node:fs'
 
 function parseLine(line) {
   let res = /^(\ *)- \[(.*)\]\((.*)\)/.exec(line)
-  if(res) {
+  if (res) {
     return {
       space: res[1].length,
-      text : res[2],
-      link : res[3]
+      text: res[2],
+      link: res[3]
     }
   }
   res = /^(\ *)- (.*)/.exec(line)
-  if(res) {
+  if (res) {
     return {
       space: res[1].length,
       text: res[2]
@@ -21,8 +21,8 @@ function parseLine(line) {
 }
 // mdPath: the path of markdown file
 // rootPath: 
-export function parseBar(mdPath, rootPath='/', autoSetCollapsible=true, indent=2) {
-  const mdSrc = fs.readFileSync(mdPath, {encoding:'utf8'})
+export function parseBar(mdPath, rootPath = '/', autoSetCollapsible = true, indent = 2) {
+  const lines = fs.readFileSync(mdPath, { encoding: 'utf8' }).split('\n')
   const dummyNode = {
     children: []
   }
@@ -33,35 +33,39 @@ export function parseBar(mdPath, rootPath='/', autoSetCollapsible=true, indent=2
   // - the 1-st level
   const lastNodes = [dummyNode]
   let lastLevel = 0
-  for(let line of mdSrc.split('\n')) {
+  for (let i = 0; i < lines.length; ++i) {
+    const line = lines[i]
+    if (line.length === 0) {
+      continue
+    }
     // parse
     const res = parseLine(line)
     const level = res ? res.space / indent + 1 : null
-    if(!level || !Number.isInteger(level) || level > lastLevel + 1) {
-      console.log('Error Line: ', line)
+    if (!level || !Number.isInteger(level) || level > lastLevel + 1) {
+      console.log(`vitepress-parse-bar: error ${i}-th line ('${line}') of ${mdPath}`)
       continue
     }
     const node = {
       text: res.text,
       children: []
     }
-    if(res.link) {
+    if (res.link) {
       res.link = res.link.replace('\\', '/') // preprocess
       node.link = path.posix.resolve(rootPath, res.link)
-      if(res.link.endsWith('/')) {
+      if (res.link.endsWith('/') && node.link !== '/') {
         node.link += '/'
       }
     }
     // process
     lastNodes[level - 1].children.push(node)
-    if(lastNodes.length == level) {
+    if (lastNodes.length == level) {
       // create the new level
       lastNodes.push(node)
     } else {
       // remove the empty children
-      if(lastNodes[level].children.length == 0) {
+      if (lastNodes[level].children.length == 0) {
         delete lastNodes[level].children
-      } else if(autoSetCollapsible){
+      } else if (autoSetCollapsible) {
         node.collapsible = true
       }
       // update the lastNodes
@@ -70,11 +74,11 @@ export function parseBar(mdPath, rootPath='/', autoSetCollapsible=true, indent=2
     lastLevel = level
   }
   // remove the empty children
-  for(let i = 1; i < lastNodes.length; ++i) {
+  for (let i = 1; i < lastNodes.length; ++i) {
     const node = lastNodes[i]
-    if(node.children.length == 0) {
+    if (node.children.length == 0) {
       delete node.children
-    } else if(autoSetCollapsible){
+    } else if (autoSetCollapsible) {
       node.collapsible = true
     }
   }
@@ -84,15 +88,14 @@ export function parseBar(mdPath, rootPath='/', autoSetCollapsible=true, indent=2
 export function scanBarFile(workspace, rootPath, barFileName) {
   const ret = {} // logicalPath : content of physicalPath
   const helper = (physicalPath, logicalPath) => {
-    for(let file of fs.readdirSync(physicalPath)) {
+    for (let file of fs.readdirSync(physicalPath)) {
       const pPath = path.join(physicalPath, file),
-            lPath = path.posix.join(logicalPath, file)
-      if(fs.statSync(pPath).isDirectory()) {
+        lPath = path.posix.join(logicalPath, file)
+      if (fs.statSync(pPath).isDirectory()) {
         helper(pPath, lPath)
-      } else if(file == barFileName) {
-        // console.log(pPath, lPath)
-        console.log(logicalPath)
-        ret[logicalPath + '/'] = parseBar(pPath, logicalPath)
+      } else if (file == barFileName) {
+        const key = logicalPath === '/' ? logicalPath : logicalPath + '/'
+        ret[key] = parseMd(pPath, logicalPath)
       }
     }
   }
